573eb7614988a28fce5b81e42129da0156c36158
samoatesgames <sam@samoatesgames.com>
2014-08-23 12:09:35 +0100

    [Add] Keyboard and mouse profile.
    [Add] Managers prefab.

---
+++ Unity/Assets/InControl/Profiles/KeyboardMouseProfile.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using InControl;
+using System.Collections;
+
+public class KeyboardMouseProfile : UnityInputDeviceProfile
+{
+
+    public KeyboardMouseProfile()
+    {
+        Name = "Keyboard/Mouse";
+        Meta = "Keyboard and mouse input handling";
+
+        SupportedPlatforms = new[] 
+        {
+            "Windows",
+            "Mac",
+            "Linux"
+        };
+
+        Sensitivity = 1.0f;
+        LowerDeadZone = 0.0f;
+        UpperDeadZone = 1.0f;
+
+        AnalogMappings = new[]
+        {
+            new InputControlMapping
+            {
+                Handle = "Move Y WASD",
+                Target = InputControlType.LeftStickY,
+                Source = KeyCodeAxis( KeyCode.S, KeyCode.W )
+            },
+            new InputControlMapping {
+              Handle = "Move X WASD",
+              Target = InputControlType.LeftStickX,
+              Source = KeyCodeAxis( KeyCode.A, KeyCode.D )
+            }
+        };
+    }
+
+}
